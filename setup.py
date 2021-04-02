c2f4cd0855b67b650e43002a00a9055474900184
Pankaj <pankajthekush@gmail.com>
2021-04-02 14:33:13 +0000

    update

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
-
+from glob import glob
+import os
+current_path = os.path.dirname(os.path.realpath(__file__))
 
 setup(
-    name = 'useragentlists',
+    name = 'uagents',
     version='1.0',
-    packages=['uagents']
+    packages=['uagents'],
+    include_package_data=True,
+    package_data = {"uagents": ["json/*.json"],}
+
 )
