9c9080a99e4307de653a251d8c1332949978524e
pankaj <pankajthekush@gmail.com>
2022-11-26 15:03:41 +0530

    include package data

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ current_path = os.path.dirname(os.path.realpath(__file__))
 
 setup(
     name = 'uagents',
-    version='1.1',
+    version='1.2',
     packages=['uagents'],
     include_package_data=True,
-    package_data = {"uagents": ["json/*.json"],}
+    package_data = {"uagents": ["json/*.json"],"uagents": ["csv/*.csv"] }
 
 )
